7e1ba5fccb98a508eaeec40dc255e9d2048deebc
xlxs4 <orousoultzoglou@gmail.com>
2023-04-19 20:40:20 +0300

    chapter 11: hot dogs

---
+++ playsync/src/playsync/core.clj
@@ -0,0 +1,21 @@
+(ns playsync.core
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]))
+
+(defn hot-dog-machine
+  [hot-dog-count]
+  (let [in (chan)
+        out (chan)]
+    (go (loop [hc hot-dog-count]
+          (if (> hc 0)
+            (let [input (<! in)]
+              (if (= 3 input)
+                (do (>! out "hot dog")
+                    (recur (dec hc)))
+                (do (>! out "wilted lettuce")
+                    (recur hc))))
+            (do (close! in)
+                (close! out)))))
+    [in out]))
